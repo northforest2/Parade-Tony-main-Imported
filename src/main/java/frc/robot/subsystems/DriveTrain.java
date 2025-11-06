e3eb85c2f121d5849fa78fca53df1fe4b9c04525
Bruce Jackson <brucejackson1000@gmail.com>
2025-11-06 18:53:58 -0500

    fixed the drivetrain

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -11,67 +11,54 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 
 import com.revrobotics.spark.config.LimitSwitchConfig.Type;
 
-
-
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-public  class DriveTrain extends SubsystemBase{
+public class DriveTrain extends SubsystemBase {
     private final SparkMax leftLeader = new SparkMax(1, MotorType.kBrushless);
     private final SparkMax rightLeader = new SparkMax(3, MotorType.kBrushless);
     private final SparkMax leftFollower = new SparkMax(2, MotorType.kBrushless);
     private final SparkMax rightFollower = new SparkMax(4, MotorType.kBrushless);
 
-    
+    private final DifferentialDrive drive = new DifferentialDrive(leftLeader, rightLeader);
 
-    private final DifferentialDrive drive = new DifferentialDrive(leftLeader, rightLeader);  
-    
-    //  
+    //
 
-    public DriveTrain(){
+    public DriveTrain() {
 
-    SparkMaxConfig globalConfig = new SparkMaxConfig();
-    SparkMaxConfig RightLeaderConfig = new SparkMaxConfig();
-    SparkMaxConfig LeftFollowerConfig = new SparkMaxConfig();
-    SparkMaxConfig RightFollowerConfig = new SparkMaxConfig();
+        SparkMaxConfig globalConfig = new SparkMaxConfig();
+        SparkMaxConfig RightLeaderConfig = new SparkMaxConfig();
+        SparkMaxConfig LeftFollowerConfig = new SparkMaxConfig();
+        SparkMaxConfig RightFollowerConfig = new SparkMaxConfig();
 
-    globalConfig
-        .smartCurrentLimit(50)
-        .idleMode(IdleMode.kCoast);
+        globalConfig
+                .smartCurrentLimit(50)
+                .idleMode(IdleMode.kCoast);
 
-    RightLeaderConfig
-        .apply(globalConfig)
-        .inverted(true);
+        RightLeaderConfig
+                .apply(globalConfig)
+                .inverted(true);
 
-    LeftFollowerConfig
-        .apply(globalConfig)
-        .follow(leftLeader);
+        LeftFollowerConfig
+                .apply(globalConfig)
+                .follow(leftLeader);
 
-    RightFollowerConfig
-        .apply(globalConfig)
-        .follow(rightLeader);
+        RightFollowerConfig
+                .apply(globalConfig)
+                .follow(rightLeader);
 
-
-    leftLeader.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    rightLeader.configure(RightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    leftFollower.configure(LeftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    rightFollower.configure(RightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    
+        leftLeader.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        rightLeader.configure(RightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        leftFollower.configure(LeftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        rightFollower.configure(RightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
     }
 
-    public void arcadeDrive(double fwd, double rot){
+    public void arcadeDrive(double fwd, double rot) {
         drive.arcadeDrive(fwd, rot);
     }
 
-    public void stop(){
+    public void stop() {
         drive.stopMotor();
     }
-
-
-    
-
-
-
-
 
 }
